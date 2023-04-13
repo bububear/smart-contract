06cb1d9abb9694a73a6424f98c9f67b988288cf0
bububear <anthony_zcc@163.com>
2023-04-13 10:30:12 +0800

    from remix ide

--- Helloword.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.6;
-
-contract Helloworld {
-    string name = "bububear";
-    function getName() public view returns(string memory){return name;}
-}
