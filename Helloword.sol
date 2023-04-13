3535f9dce73cda5e1f4924f71ca250a7e5526aef
anthony_zcc <anthony_zcc@163.com>
2023-04-13 13:11:25 +0800

    dada

--- Helloword.sol
+++ Helloword.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 
 contract Hellowold{
     string name = "bububear";
-    // test 
+    // test dad
     function getName() public view returns(string memory){
         return name;
     }
