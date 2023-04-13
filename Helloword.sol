8ff5d592107f234d8cdce9d4948efb1fa94fac38
anthony_zcc <anthony_zcc@163.com>
2023-04-13 10:26:11 +0800

    date

--- Helloword.sol
+++ Helloword.sol
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.6;
 
-contract Helloworld{
-
-string name = "bububear";
-
+contract Helloworld {
+    string name = "bububear";
+    function getName() public view returns(string memory){return name;}
 }
